00c887af8dc9cc45298e99ae0e6294112ab502ac
Moses Borore <mosesbororekinuthia@gmail.com>
2022-03-24 21:19:52 +0300

    add column and row select

--- static/datatables/main.js
+++ static/datatables/main.js
@@ -1,8 +1,16 @@
 $(document).ready(function () {
    $("#table_id").DataTable({
+    columnDefs:[{
+      orderable: false,
+      className: 'focusedRow',
+      target: 0
+    }],
+    select:{
+      style: 'multi',
+    },
+    order:[[1, 'asc']],
     scrollX: true,
     scrollY: "70vh",
-    select:true,
     scrollCollapse: true,
     //responsive: true,
     paging: true,
@@ -13,7 +21,11 @@ $(document).ready(function () {
         text: "Export PDF",
         extend: "pdfHtml5",
         messageTop: "Farm information report",
+        exportOptions: {
+          columns: [':visible']
+        }
       },
+      'colvis'
     ],
   });
 });
