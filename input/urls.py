1616d61e74269d50a23b5011873b70e50c2ee9ce
Moses Borore <mosesbororekinuthia@gmail.com>
2022-04-02 17:06:12 +0300

    add inventory and items functionality

--- input/urls.py
+++ input/urls.py
@@ -5,9 +5,19 @@ from . import views
 app_name = "input"
 
 urlpatterns = [
-    path("", views.input_home, name="input_home"),
+    path("inventory/", views.input_home, name="input_home"),
+    path(
+        "inventory/<str:ref_code>/delete",
+        views.delete_inventory,
+        name="delete-inventory",
+    ),
+    path(
+        "inventory/<str:ref_code>",
+        views.inventory_item_list,
+        name="inventory-item-list",
+    ),
     path("products", views.Products.as_view(), name="product-list"),
     path("product/add", views.add_product, name="add-product"),
-    path("product/edit/<int:pk>", views.edit_product, name="edit-product"),
-    path("product/delete/<int:pk>", views.delete_product, name="delete-product"),
+    path("product/<int:pk>/edit", views.edit_product, name="edit-product"),
+    path("product/<int:pk>/delete", views.delete_product, name="delete-product"),
 ]
--- input/views.py
+++ input/views.py
@@ -1,13 +1,22 @@
+from email import message
+
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Count
+from django.http import Http404
 from django.shortcuts import get_object_or_404, redirect, render
 from django.utils.translation import gettext_lazy as _
 from django.views.decorators.http import require_POST
 from django.views.generic import ListView
+from django_prices.models import MoneyField
 
-from .forms import InputCategoryForm, InputInventoryForm, InputProductForm
+from .forms import (
+    InputCategoryForm,
+    InputInventoryForm,
+    InputInventoryItemForm,
+    InputProductForm,
+)
 from .models import InputInventory, InputProduct
 
 
@@ -16,10 +25,66 @@ from .models import InputInventory, InputProduct
 def input_home(request):
     # products = InputProduct.objects.all()
     # count items per inventory
-    inventories = InputInventory.objects.all().annotate(item_count=Count("items"))
+    inventories = InputInventory.objects.prefetch_related("items").annotate(
+        item_count=Count("items")
+    )
 
     context = {"inventories": inventories}
     return render(request, "input/index.html", context)
+
+
+@login_required(login_url="/account/login")
+def delete_inventory(request, ref_code):
+    delete = request.POST.get("delete")
+
+    if delete:
+        # if the checkbox is checked
+        inventory = get_object_or_404(InputInventory, ref_code=ref_code)
+        name = inventory.name
+        inventory.delete()
+        messages.success(request, f"{name} delete successfully")
+    else:
+        messages.info(
+            request,
+            _("If you want to delete the record, make sure the checkbox is checked"),
+        )
+    return redirect("input:input_home")
+
+
+@login_required(login_url="/account/login/")
+def inventory_item_list(request, ref_code):
+
+    try:
+        inventory = get_object_or_404(InputInventory, ref_code=ref_code)
+        items = inventory.items.select_related("input_product").all()
+
+        if request.method == "POST":
+            new_item_form = InputInventoryItemForm(request.POST)
+            if new_item_form.is_valid():
+                new_item_form.save()
+                messages.success(request, "New Inventory added successfully")
+            else:
+                messages.error(request, new_item_form.errors.as_ul())
+        if request.method == "DELETE":
+            print("delete meho")
+
+    except Http404:
+        messages.error(
+            request, f'Inventory with reference code: "{ref_code}" does not exist'
+        )
+        return redirect("input:input_home")
+
+    form = InputInventoryItemForm()
+
+    context = {
+        "items": items,
+        "inventory_name": inventory.name,
+        "ref_code": ref_code,
+        "currency": "KSH",
+        "form": form,
+        "inventory_total_cost": inventory.get_total_cost(),
+    }
+    return render(request, "input/inventory-item-list.html", context)
 
 
 class Products(LoginRequiredMixin, ListView):
